2880bb56fae72db887fc0bbb4f4154df9da633d4
felipeomelo <felipe.o.melo@hotmail.com>
2024-10-31 11:13:04 -0300

    Commit inicial

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import express  from "express"
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.json('OK!')
+}) 
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor inicializado!');
+    
+})
